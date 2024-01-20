a944f6ec9c99b3bdf03d1a72cdbf970f06ae48fd
Satyam Kumar <satyamsingh748846@gmail.com>
2024-01-20 23:31:51 +0530

    new

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+  });
+
+
 
 app.use(cors({
 	origin: ["https://fakeapi-lete.onrender.com/"],
